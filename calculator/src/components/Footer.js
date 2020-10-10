b6ff7ffd0b1c7bb56e3703d1f4deb05265bce160
Decio <decio.msoares@gmail.com>
2020-10-10 11:37:50 +0100

    add changes to index.css

---
+++ calculator/src/components/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <div>
+        Made by{" "}
+        <span>
+          <a href="https://codepen.io/dmsoares">Decio</a>
+        </span>{" "}
+        for the <a href="https://freecodecamp.org">FCC</a> JavaScript Calculator
+        Challenge
+      </div>
+      <div>
+        Design inspired by
+        <a href="https://dribbble.com/shots/12501033-Calculator-Daily-UI-004">
+          {" "}
+          Isha Godara
+        </a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
